357860fa5ebd092d11254965af8d7e860b61127a
LuisFuentesDev <lsfuentes2@gmail.com>
2023-07-28 22:05:46 -0400

    hito 3

---
+++ app/src/main/java/com/example/checkregister/presentation/AddFragment.kt
@@ -0,0 +1,19 @@
+package com.example.checkregister.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.checkregister.R
+
+
+class AddFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_add, container, false)
+    }
+}
