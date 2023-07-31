65103ee9c8fd53e24df99b161df27b9a7f761609
LuisFuentesDev <lsfuentes2@gmail.com>
2023-07-31 19:55:12 -0400

    guarda la info

--- app/src/main/java/com/example/checkregister/presentation/AddFragment.kt
+++ app/src/main/java/com/example/checkregister/presentation/AddFragment.kt
@@ -5,15 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.example.checkregister.R
+import com.example.checkregister.databinding.ActivityMainBinding
+import com.example.checkregister.databinding.FragmentAddBinding
 
 
 class AddFragment : Fragment() {
+
+    lateinit var binding: FragmentAddBinding
+    private val itemsViewModel: ItemsViewModel by activityViewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        binding = FragmentAddBinding.inflate(layoutInflater, container, false)
+
+        initListener()
+
+        return binding.root
+    }
+
+    private fun initListener() {
+        binding.buttonGuardar.setOnClickListener() {
+            val nombre = binding.editTextNombre.text.toString()
+            val cantidad = binding.editTextCantidad.text.toString().toInt()
+            val precio = binding.editTextPrecio.text.toString().toInt()
+
+            itemsViewModel.insertItem(nombre,cantidad,precio)
+
+        }
     }
 }
